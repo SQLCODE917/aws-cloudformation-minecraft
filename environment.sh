5b72abc2546e0f7d451e6d7814e9b194c1c1ecc2
Finntaur <finntaur@gmail.com>
2022-07-23 14:51:46 +0300

    FIXED: Environment init script failed to execute
    - AWS region wasn't provided to the commands when needed
    - Cloudformation AWS CLI command was accidentally outputting text instead of JSON

--- environment.sh
+++ environment.sh
@@ -11,16 +11,24 @@ minecraft_log "Initializing Bedrock server environment at $(date)"
 # ---------------------------------------------------------------------------------------
 
 INSTANCE_ID="$(curl "http://169.254.169.254/latest/meta-data/instance-id")"
+AWS_REGION="$(curl -q "http://169.254.169.254/latest/dynamic/instance-identity/document" | jq -r '.region')"
 
 minecraft_log "Fetching instance details"
-STACK_NAME="$(aws ec2 describe-tags --filters \
-    "Name=resource-id,Values=${INSTANCE_ID}" \
-    "Name=key,Values=aws:cloudformation:stack-name" \
-    "Name=resource-type,Values=instance" --query 'Tags[0].Value' --output text)"
+STACK_NAME="$(aws ec2 describe-tags \
+    --region "${AWS_REGION}" \
+    --filters \
+        "Name=resource-id,Values=${INSTANCE_ID}" \
+        "Name=key,Values=aws:cloudformation:stack-name" \
+        "Name=resource-type,Values=instance" \
+    --query 'Tags[0].Value' \
+    --output text)"
 
 minecraft_log "Reading configuration parameters"
-PARAMETERS="$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" \
-    --output text --query 'Stacks[0].Parameters')"
+PARAMETERS="$(aws cloudformation describe-stacks \
+    --region "${AWS_REGION}" \
+    --stack-name "${STACK_NAME}" \
+    --output json \
+    --query 'Stacks[0].Parameters')"
 
 BEDROCK_ROOT_DIR="/usr/games/minecraft"
 
