782ede4e8773eb4ff61c54cd45c639fa53c9ba10
Finntaur <finntaur@gmail.com>
2022-07-23 13:41:41 +0300

    Script for backing up Minecraft worlds to S3

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /usr/games/minecraft-environment
+
+function backup_minecraft_worlds() {
+    local backup_filename="/tmp/worlds.tar.bz2"
+    local bucket_name="minecraft-$(get_parameter UUID)"
+
+    rm -f "${backup_filename}"
+    tar -c -j -f "${backup_filename}" -C "${BEDROCK_ROOT_DIR}"
+    aws s3 cp "${backup_filename}" "s3://${bucket_name}/backups/" && rm -f "${backup_filename}"
+}
+
+# ---------------------------------------------------------------------------------------
+
+minecraft_log "Backing up Minecraft worlds at $(date)"
+backup_minecraft_worlds
+minecraft_log "Backups completed at $(date)"
